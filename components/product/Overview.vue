40602501036cc2e657df484063c50bcc9452e5b5
wojciechOz <wojciech.rosinski@ozparts.eu>
2024-03-15 20:54:16 +0100

    fix(Overview): fix overview styles

--- components/product/Overview.vue
+++ components/product/Overview.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="px-2 mx-auto mb-10 overflow-hidden">
+  <div class="mx-auto mb-10 overflow-hidden">
     <div
       v-if="product.detaildescription"
       v-html="product.detaildescription"
-      class="overview mx-auto px-3 py-5 font-nunito text-sm text-[#151515]"
+      class="mx-auto py-5 font-nunito text-base text-[#151515]"
+      :class="styleClass"
     ></div>
     <ProductKitComponents
       v-if="
@@ -20,14 +21,95 @@
 <script setup>
 import { Manufacturers } from "../../vars/index";
 
-const props = defineProps(["product"]);
+const { product } = defineProps(["product"]);
+
+const styleClass = computed(() => {
+  return {
+    dba: `${product.manufacturergroup}` === `${Manufacturers.DBA.id}`,
+    "dba-street":
+      product.description.includes("Brake Pads Street") &&
+      product.manufacturergroup === Manufacturers.DBA.id,
+    "dba-xtreme":
+      product.description.includes("Brake Pads Xtreme") &&
+      product.manufacturergroup === Manufacturers.DBA.id,
+    xtreme: product.manufacturergroup === Manufacturers.ACS.id,
+    "xtreme-230mm-organic":
+      product.manufacturergroup === Manufacturers.ACS.id &&
+      product.description.includes("230mm Organic Twin"),
+    "xtreme-230mm-carbon":
+      product.manufacturergroup === Manufacturers.ACS.id &&
+      product.description.includes("230mm Carbon Blade"),
+    "xtreme-230mm-rigid":
+      product.manufacturergroup === Manufacturers.ACS.id &&
+      product.description.includes("230mm Rigid Ceramic"),
+    "xtreme-270mm-organic":
+      product.manufacturergroup === Manufacturers.ACS.id &&
+      product.description.includes("270mm Organic Twin"),
+    "xtreme-230mm-sprung":
+      product.manufacturergroup === Manufacturers.ACS.id &&
+      product.description.includes("230mm Sprung Ceramic"),
+    xforce: `${product.manufacturergroup}` === `${Manufacturers.XFORCE.id}`,
+    whiteline:
+      `${product.manufacturergroup}` === `${Manufacturers.WHITELINE.id}`,
+    pedders: `${product.manufacturergroup}` === `${Manufacturers.PEDDERS.id}`,
+    acl: `${product.manufacturergroup}` === `${Manufacturers.ACL.id}`,
+  };
+});
 </script>
 
 <style scoped>
-div:deep(.overview) {
+div:deep(.dba-street) {
+  @media (width <= 640px) {
+    img + img {
+      content: url("https://ozparts.eu/dba-street-performance-pads.jpg");
+      width: 100%;
+      margin-bottom: 16px;
+      margin-top: 16px;
+      max-height: 400px;
+      float: none !important;
+    }
+  }
+  @media (width > 640px) {
+    img + img {
+      content: url("https://ozparts.eu/dba-street-performance-pads.jpg");
+      float: right !important;
+      width: 80%;
+      max-height: 400px;
+    }
+  }
+}
+div:deep(.dba-xtreme) {
+  @media (width <= 640px) {
+    p + img {
+      content: url("https://ozparts.eu/dba-xtreme-performance-pads.png");
+      width: 100%;
+      margin-bottom: 16px;
+      margin-top: 16px;
+      max-height: 400px;
+      float: none !important;
+    }
+  }
+  @media (width > 640px) {
+    p + img {
+      content: url("https://ozparts.eu/dba-xtreme-performance-pads.png");
+      float: right !important;
+      width: 80%;
+      max-height: 400px;
+    }
+  }
+}
+div:deep(.dba) {
+  p {
+    margin-bottom: 16px;
+  }
   p > img {
     width: 90% !important;
-    margin: 0 auto;
+    margin: 10px auto;
+  }
+  ul {
+    list-style: disc;
+    margin-top: 10px;
+    padding-left: 20px;
   }
   iframe {
     max-width: 90%;
@@ -43,6 +125,7 @@ div:deep(.overview) {
   table > tbody {
     display: flex;
     flex-direction: column;
+    line-height: normal;
   }
 
   /* header - row*/
@@ -150,6 +233,162 @@ div:deep(.overview) {
       text-transform: uppercase;
       margin-bottom: 10px;
     }
+  }
+}
+div:deep(.xtreme) {
+  p {
+    margin-bottom: 16px;
+  }
+  ul {
+    list-style: disc;
+    margin-top: 10px;
+    padding-left: 20px;
+  }
+}
+div:deep(.xtreme-230mm-organic),
+div:deep(.xtreme-270mm-organic) {
+  img {
+    margin-top: 0 !important;
+  }
+}
+div:deep(.xtreme-230mm-carbon) {
+  table > tbody > tr > td > h1 {
+    font-size: 16px !important;
+  }
+  table#xoverview > tbody > tr:nth-of-type(1) {
+    display: none;
+  }
+  table#xoverview > tbody > tr:nth-of-type(2) > td:nth-of-type(1) {
+    display: none;
+  }
+  table#xoverview > tbody > tr:nth-of-type(3) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  table#xoverview > tbody > tr:nth-of-type(3) > td:nth-of-type(2) {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  }
+  table#xoverview > tbody > tr:nth-of-type(3) > td:nth-of-type(2) img {
+    flex-basis: 40%;
+    padding: 10px;
+    margin: 0 auto;
+  }
+  table#xoverview > tbody > tr:nth-of-type(3) > td:nth-of-type(2) h3 {
+    flex-basis: 100%;
+    font-size: 20px;
+    margin: 10px 0;
+  }
+  table#xoverview > tbody > tr:nth-of-type(3) > td:nth-of-type(2) ul > li {
+    font-size: 16px;
+  }
+}
+div:deep(.xtreme-230mm-rigid) {
+  @media (width <= 640px) {
+    b > div:nth-of-type(1) {
+      flex-direction: column;
+    }
+    b > div:nth-of-type(1) > img {
+      height: 150px !important;
+      width: 150px !important;
+      align-self: center;
+    }
+
+    b > div:nth-of-type(1) > div:nth-of-type(1) {
+      width: 100% !important;
+      flex-basis: 100% !important;
+    }
+    b > div:nth-of-type(1) > div:nth-of-type(2) > img {
+      width: 50% !important;
+      align-self: center;
+    }
+    b > div:nth-of-type(2) {
+      flex-direction: column;
+      margin: 10px 0;
+    }
+    b > div:nth-of-type(2) > div {
+      margin: 10px auto;
+      order: 1;
+    }
+    b > div:nth-of-type(2) > table {
+      margin-top: 10px;
+    }
+  }
+  @media (width > 640px) {
+    b > div:nth-of-type(1) {
+      align-items: center;
+    }
+    b > div:nth-of-type(1) > div:nth-of-type(1) {
+      width: 50% !important;
+    }
+    b > div:nth-of-type(1) > img {
+      width: 25% !important;
+      max-height: 300px !important;
+    }
+    b > div:nth-of-type(1) > div:nth-of-type(2) > img {
+      width: 50% !important;
+      height: 300px;
+    }
+  }
+}
+div:deep(.xtreme-230mm-sprung) {
+  b > div:nth-of-type(1) > div:nth-of-type(2) > img {
+    width: 45% !important;
+    margin: 10px auto;
+  }
+  @media (width <= 640px) {
+    b > div:nth-of-type(1) {
+      flex-direction: column;
+    }
+  }
+}
+div:deep(.xforce) {
+  ul {
+    list-style: disc;
+    margin-top: 10px;
+    padding-left: 20px;
+  }
+  iframe {
+    max-width: 90%;
+    margin: 0 auto;
+  }
+}
+div:deep(.pedders) {
+  ul {
+    list-style: disc;
+    margin: 10px 0;
+    padding-left: 20px;
+  }
+  p,
+  h2,
+  h3 {
+    margin-bottom: 16px;
+  }
+  h2,
+  h3 {
+    font-weight: bold;
+  }
+  iframe {
+    max-width: 90%;
+    margin: 0 auto;
+  }
+}
+div:deep(.acl) {
+  ul {
+    list-style: disc;
+    margin: 10px 0;
+    padding-left: 20px;
+  }
+  p,
+  h2,
+  h3 {
+    margin-bottom: 16px;
+  }
+  h2,
+  h3 {
+    font-weight: bold;
   }
 }
 </style>
