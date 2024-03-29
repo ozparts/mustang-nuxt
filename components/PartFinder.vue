2a0d0c86a9bd30663a1770f7c630240220bee893
wojciechOz <wojciech.rosinski@ozparts.eu>
2024-03-29 23:47:53 +0100

    fix: fix link

--- components/PartFinder.vue
+++ components/PartFinder.vue
@@ -1,15 +1,15 @@
 <template>
   <div class="absolute top-[12px] right-[15px] hidden md:!block">
     <div
-      @click="state.search = true"
+      @click="() => navigateTo(`/catalogue`)"
       class="relative cursor-pointer inline-block px-4 py-2.5 text-lg font-bold tracking-[3.5px] text-white uppercase bg-red-600/80 font-nunito"
     >
       part finder
     </div>
   </div>
-  <Transition>
+  <!-- <Transition>
     <Search v-show="state.search" @close="state.search = false" />
-  </Transition>
+  </Transition> -->
 </template>
 
 <script setup>
