7dc72a957bb09bd895acc5c63e3854f05d327452
wojciechOz <wojciech.rosinski@ozparts.eu>
2024-04-22 14:30:01 +0200

    fix: resize main banner photo

--- components/home/MainBanner.vue
+++ components/home/MainBanner.vue
@@ -7,12 +7,13 @@
       <figure
         class="flex max-h-[500px] items-end justify-center overflow-hidden"
       >
+        <!-- sizes="xs:320px sm:640px md:768px lg:1200px" -->
         <nuxt-img
           format="webp"
           alt="Mustang Performance"
           src="/mustang/mustang_banner.jpg"
           provider="cloudinary"
-          sizes="xs:320px sm:640px md:768px lg:1024px xl:1280px xxl:1536px 2xl:100vw"
+          width="1150px"
           fit="contain"
         />
       </figure>
