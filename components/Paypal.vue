916af2efa0d14f38bf2bc584f389048efc891629
wojciechOz <wojciech.rosinski@ozparts.eu>
2024-05-18 18:00:29 +0200

    fix(Paypal): fix paypal token

--- components/Paypal.vue
+++ components/Paypal.vue
@@ -16,18 +16,29 @@ const state = reactive({
   order: null,
 });
 
+const runtimeConfig = useRuntimeConfig();
+
 onMounted(async () => {
   const order = await useGetOrder(route.params.id, country.code);
   state.order = order;
-  const src =
-    host === "UK"
-      ? `https://www.paypal.com/sdk/js?client-id=Ae922jVwwFJpiabY1Yphr1OsTPNjVkmvSjXnwR44b4Py2XUA2yNQWTw1yGqIX55GhzJuu0D5QtE4z7ye&disable-funding=card&currency=GBP`
-      : `https://www.paypal.com/sdk/js?client-id=Abr3Oy3T51DwdxwyKNM2X3uOiuyVAcQ9iZnnO93CY7riR-AOQsxDEs7wpIcH3mZIs1Rnda4DkAFJNKe5&disable-funding=card&currency=EUR`;
+  const currency = order.currency.name;
+  let token = "";
+  if (currency === "GBP") {
+    token = runtimeConfig.public.paypal_UK;
+  } else if (currency === "PLN") {
+    token = runtimeConfig.public.paypal_PL;
+  } else {
+    token = runtimeConfig.public.paypal_EU;
+  }
+
+  const src = `https://www.paypal.com/sdk/js?client-id=${token}&disable-funding=card&currency=${currency}`;
+
   const script = document.createElement("script");
   script.src = src;
   script.addEventListener("load", setLoaded);
   document.body.appendChild(script);
 
+  console.log(src, currency, "!!");
   function setLoaded() {
     const value = state.order.grossamount;
     const object =
@@ -47,7 +58,7 @@ onMounted(async () => {
               {
                 amount: {
                   value,
-                  currency_code: "EUR",
+                  currency_code: state.order.currency.name,
                 },
               },
             ],
