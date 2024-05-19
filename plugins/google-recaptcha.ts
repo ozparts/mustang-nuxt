b4141ee5afdeac815031e6467ecef9583b8c3fb1
wojciechOz <wojciech.rosinski@ozparts.eu>
2024-05-20 01:42:06 +0200

    fix: change key source

--- plugins/google-recaptcha.ts
+++ plugins/google-recaptcha.ts
@@ -2,10 +2,10 @@ import { VueReCaptcha } from "vue-recaptcha-v3";
 import { RECAPTCHA } from "./../vars/index";
 
 export default defineNuxtPlugin((nuxtApp) => {
-  const runtimeConfig = useRuntimeConfig();
-  console.log(runtimeConfig.public, "google-recap");
+  // const runtimeConfig = useRuntimeConfig();
+  // console.log(runtimeConfig.public, "google-recap");
   nuxtApp.vueApp.use(VueReCaptcha, {
-    siteKey: runtimeConfig.public.recaptcha_site_key || RECAPTCHA.SITE_KEY,
+    siteKey: RECAPTCHA.SITE_KEY,
     loaderOptions: {
       autoHideBadge: false,
       explicitRenderParameters: {
