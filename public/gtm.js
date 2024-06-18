3ee966d7074513f68b1fdd9912da0d0e105d0f79
wojciechOz <wojciech.rosinski@ozparts.eu>
2024-06-18 09:16:19 +0200

    fix: change gtm script

--- public/gtm.js
+++ public/gtm.js
@@ -19,7 +19,7 @@ gtag("consent", "default", {
   var f = d.getElementsByTagName(s)[0],
     j = d.createElement(s),
     dl = l != "dataLayer" ? "&l=" + l : "";
-  j.async = true;
+  j.defer = true;
   j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
   f.parentNode.insertBefore(j, f);
 })(window, document, "script", "dataLayer", "GTM-KWZSWWQV");
