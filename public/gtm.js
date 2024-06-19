95856e73994e89a3871becc9041a7c4007f295a9
wojciechOz <wojciech.rosinski@ozparts.eu>
2024-06-19 10:39:54 +0200

    test: change gtm load function

--- public/gtm.js
+++ public/gtm.js
@@ -13,18 +13,41 @@ gtag("consent", "default", {
 });
 
 // Google Tag Manager
-setTimeout(() => {
-  (function (w, d, s, l, i) {
-    w[l] = w[l] || [];
-    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-    var f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s),
-      dl = l != "dataLayer" ? "&l=" + l : "";
-    j.defer = true;
-    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+
+// (function (w, d, s, l, i) {
+//   w[l] = w[l] || [];
+//   w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+//   var f = d.getElementsByTagName(s)[0],
+//     j = d.createElement(s),
+//     dl = l != "dataLayer" ? "&l=" + l : "";
+//   j.async = true;
+//   j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+//   f.parentNode.insertBefore(j, f);
+// })(window, document, "script", "dataLayer", "GTM-KWZSWWQV");
+
+(function (w, d, s, l, i) {
+  w[l] = w[l] || [];
+  w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+
+  // Preload the script
+  var link = d.createElement("link");
+  link.rel = "preload";
+  link.as = "script";
+  link.href = "https://www.googletagmanager.com/gtm.js?id=" + i;
+  d.head.appendChild(link);
+
+  // Attach the script after preloading
+  link.onload = function () {
+    var f = d.getElementsByTagName(s)[0];
+    var j = d.createElement(s);
+    var dl = l != "dataLayer" ? "&l=" + l : "";
+    // j.defer = true;
+    j.async = true;
+    j.src = link.href;
     f.parentNode.insertBefore(j, f);
-  })(window, document, "script", "dataLayer", "GTM-KWZSWWQV");
-}, 2000);
+  };
+})(window, document, "script", "dataLayer", "GTM-KWZSWWQV");
+
 // End Google Tag Manager
 
 function consentGrantedAdStorage() {
