02ddb2a443a5ad9daa459bd2b586ae9b5c41f9ed
wojciechOz <wojciech.rosinski@ozparts.eu>
2024-07-21 18:23:50 +0200

    fix: fix image styles

--- components/Header/Mobile.vue
+++ components/Header/Mobile.vue
@@ -2,16 +2,15 @@
   <div
     class="mx-auto md:hidden h-2 max-w-[1140px] bg-[#040503] opacity-[0.55] w-full"
   ></div>
-  <div class="flex w-full h-[50px] bg-white md:hidden">
+  <div class="flex w-full h-auto min-[400px]:!h-[70px] bg-white md:hidden">
     <div class="flex justify-between w-full">
       <div class="basis-[50%]">
         <nuxt-link to="/">
-          <div class="md:w-[200px] lg:w-[300px] h-[50px]">
+          <div class="md:w-[200px] lg:w-[300px] h-full">
             <nuxt-img
-              class="aspect-auto h-[50px]"
+              class="block xs:w-[200px] min-[400px]:!w-full p-1 h-[50px] min-[400px]:!h-[70px]"
               alt="Mustang Performance logo"
               src="/mustang_logo.webp"
-              sizes="200px lg:300px"
             />
           </div>
         </nuxt-link>
