a945a746f22a0e72de8022d0317fecf24413f876
wojciechOz <wojciech.rosinski@ozparts.eu>
2026-08-25 21:37:08 +0200

    fix: comment types

--- types/analytics.ts
+++ types/analytics.ts
@@ -1,37 +1,37 @@
-export interface DataLayerEvent {
-  event?: string;
-  source?: string;
-  search_term?: string;
-  lead_source?: string;
-  ecommerce?: {
-    transaction_id?: string;
-    currency?: string;
-    payment_type?: string;
-    shipping_tier?: string;
-    tax?: number;
-    shipping?: number;
-    value?: number;
-    item_list_name?: string;
-    items?: Array<{
-      item_id: string;
-      item_name: string;
-      item_brand: string;
-      item_category: string;
-      price: number;
-      quantity: number;
-      index?: number;
-    }>;
-  } | null;
-  user_data?: {
-    email: string;
-    phone_number: string;
-    address: {
-      first_name: string;
-      last_name: string;
-      street: string;
-      city: string;
-      postal_code: string;
-      country: string;
-    };
-  };
-}
+// export interface DataLayerEvent {
+//   event?: string;
+//   source?: string;
+//   search_term?: string;
+//   lead_source?: string;
+//   ecommerce?: {
+//     transaction_id?: string;
+//     currency?: string;
+//     payment_type?: string;
+//     shipping_tier?: string;
+//     tax?: number;
+//     shipping?: number;
+//     value?: number;
+//     item_list_name?: string;
+//     items?: Array<{
+//       item_id: string;
+//       item_name: string;
+//       item_brand: string;
+//       item_category: string;
+//       price: number;
+//       quantity: number;
+//       index?: number;
+//     }>;
+//   } | null;
+//   user_data?: {
+//     email: string;
+//     phone_number: string;
+//     address: {
+//       first_name: string;
+//       last_name: string;
+//       street: string;
+//       city: string;
+//       postal_code: string;
+//       country: string;
+//     };
+//   };
+// }
